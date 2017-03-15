9aabb61a2cacffeed7e8bfe5dc0806c53c059d5d
lorich <lyc_xjtu@163.com>
2017-03-16 00:35:23 +0800

    code layout optimize

---
+++ 451. Sort Characters By Frequency/Accepted-82ms-96838756.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        dicts=dict.fromkeys(list(set(s)), 0)
+        for c in s:
+        	dicts[c]+=1
+        return ''.join([i[0]*i[1] for i in sorted(dicts.items(), key=lambda it:it[1], reverse=True)])
+
+print(Solution().frequencySort('AbdcAaaaa'))
