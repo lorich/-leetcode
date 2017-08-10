f03b27ca0c1b2eba5b1abf179e3640bd89073318
yongchaoliu <yongchaoliu@yongchaoliudeMacBook-Pro.local>
2017-08-10 22:43:32 +0800

    518, dp

---
+++ 518. Coin Change 2/Accepted-6ms-dp.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int change(const int amount, const vector<int>& coins) {
+            vector<int> dp(amount+1, 0);
+            dp[0] = 1;
+            for(auto c : coins)
+                for (int i = 1; i <= amount; ++i)
+                {
+                    if(i >= c) dp[i] += dp[i-c];
+                }
+            return dp[amount];
+        }
+};
